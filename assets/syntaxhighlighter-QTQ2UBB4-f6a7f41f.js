c47f5c227c534af5ebe3d4d9b907d55c34c7cf8b
Kirill <qtrixnet@yandex.ru>
2023-05-02 22:44:11 +0600

    Updates

--- assets/syntaxhighlighter-QTQ2UBB4-f6a7f41f.js
+++
@@ -1,2 +0,0 @@
-import{S as n,c as d,s as f}from"./DocsRenderer-7FRJXR4N-ef6e87bc.js";import"./iframe-4fd3572a.js";import"../sb-preview/runtime.mjs";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./_getPrototype-71500a16.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-356e4a49.js";import"./preview-6bcd7fa7.js";export{n as SyntaxHighlighter,d as createCopyToClipboardFunction,f as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-f6a7f41f.js.map
