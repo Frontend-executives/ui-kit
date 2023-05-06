5048fecc6c6cc43ba1789698ea989f192bac4b32
Kirill <qtrixnet@yandex.ru>
2023-05-06 18:47:03 +0600

    feat: change bandler for storybook deploy

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,14 @@
-import type { StorybookConfig } from '@storybook/react-vite'
+import type { StorybookConfig } from '@storybook/react-webpack5'
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
     '@storybook/addon-interactions',
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: '@storybook/react-webpack5',
     options: {},
   },
   docs: {
