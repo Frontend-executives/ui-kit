ef174793c701352a6414ce3c5a9c50af54c26a54
Dastan Zhamekeshev <dastan132000@mail.ru>
2023-05-17 17:51:22 +0600

    fix: fixed Link component types, styling

--- src/components/Link/types.ts
+++ src/components/Link/types.ts
@@ -1,12 +1,6 @@
-import { ReactNode } from 'react'
-
 export interface ILinkProps {
   className?: string
-  icon?: ReactNode
   isDisabled?: boolean
   isExternal?: boolean
-  isLoading?: boolean
-  isRounded?: boolean
   text?: string
-  theme?: 'primary' | 'failed' | 'success' | 'warning'
 }
